16dd53c6aedc8583eea4ca4f5cc971c248db3d1a
nzmpi <nizami.abdulov@gmail.com>
2023-04-18 20:25:46 +0400

    v1

---
+++ README.md
@@ -0,0 +1,14 @@
+# CustomETHaddress
+
+Create an address with custom prefix
+
+## How to use
+
+ 1. `yarn add hardhat`
+
+ 2. Change `customPrefix` to a desired prefix
+
+ 3. Change `seed` to any string you want
+
+ 4. `node customAddress.js`
+
---
+++ customAddress.js
@@ -0,0 +1,66 @@
+const { BigNumber } = require("ethers");
+const { 
+  keccak256, 
+  arrayify, 
+  getAddress: checksumAddress, 
+  hexDataSlice,
+  toUtf8Bytes, 
+  hexZeroPad
+} = require("ethers/lib/utils");
+const { CURVE, Point } = require('@noble/secp256k1');
+
+/**
+ * @dev Prefix of your custom address
+ * Have to start with 0x + 0-9 and/or a-f
+ * Recommended length is 0x + 5-6 symbols or less,
+ * otherwise it may take a lot of time to find an address
+ */
+const customPrefix = "0x0dad0";
+
+/**
+ * @dev Custom seed 
+ * If it takes a lot of time to find an address, change the seed
+ */
+const seed = "Monkey See, Monkey Do 🐵";
+console.log("seed = ", seed);
+
+let privateKeyFromSeed = BigNumber.from(keccak256(toUtf8Bytes(seed)));
+let privateKey = arrayify(privateKeyFromSeed.mod(CURVE.n));
+
+/**
+ * @dev BigNumber deletes leading zeros,
+ * which makes private key invalid
+ */
+if (privateKey.length < 32) {
+  privateKey = arrayify(hexZeroPad(privateKey, 32));
+}
+let publicKey = Point.fromPrivateKey(privateKey);
+
+/**
+ * @dev For loop that will generate addresses
+ */
+for (let i = 1; ; ++i) {
+  // Print i every millionth iteration
+  if (i%1000000 === 0) {
+    console.log("i = ", i/1000000);
+  }
+
+  // PK = PK + G
+  publicKey = publicKey.add(Point.BASE);
+  
+  // Infer address from publicKey
+  let getAddress = hexDataSlice(keccak256(
+    hexDataSlice('0x' + publicKey.toHex(), 1)
+  ), 12);
+
+  // Check the address
+  if (getAddress.startsWith(customPrefix)) {
+    // Infer the private key of customAddress
+    let getPrivateKey = BigNumber.from(privateKey).add(i);
+
+    console.log("\ni = ", i);
+    console.log("Private Key = ", getPrivateKey.toHexString());
+    console.log("Address = ", checksumAddress(getAddress));
+    console.log("\n");
+  }
+}
